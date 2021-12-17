38b150190863a4a92171be4e946405788fad079a
Cédric Anne <cedric.anne@gmail.com>
2021-12-17 12:40:31 +0100

    Fix licence headers

--- setup.php
+++ setup.php
@@ -1,29 +1,30 @@
 <?php
-/*
- -------------------------------------------------------------------------
- Fields plugin for GLPI
- Copyright (C) 2016 by the Fields Development Team.
 
- https://github.com/pluginsGLPI/fields
- -------------------------------------------------------------------------
-
- LICENSE
-
- This file is part of Fields.
-
- Fields is free software; you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation; either version 2 of the License, or
- (at your option) any later version.
-
- Fields is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with Fields. If not, see <http://www.gnu.org/licenses/>.
- --------------------------------------------------------------------------
+/**
+ * -------------------------------------------------------------------------
+ * GLPI - Gestionnaire Libre de Parc Informatique
+ * Copyright (C) 2003-2021 by the Teclib Development Team.
+ *
+ * http://teclib.com/   http://glpi-project.org
+ * -------------------------------------------------------------------------
+ *
+ * LICENSE
+ *
+ * This file is part of Fields.
+ *
+ * Fields is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Fields is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Fields. If not, see <http://www.gnu.org/licenses/>.
+ * --------------------------------------------------------------------------
  */
 
 define ('PLUGIN_FIELDS_VERSION', '1.12.8');
